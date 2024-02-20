50d3e5f4bf67fee312f2f39cb90fafb36e1808fc
Tokoyamy <math.felipemachado@gmail.com>
2024-02-20 00:55:57 -0300

    Password Hashing

--- src/core/user/user.service.ts
+++ src/core/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { v1 as uuidv1 } from 'uuid';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
 
 @Injectable()
@@ -7,20 +8,25 @@ export class UserService {
   private users = [
     {
       id: 'db2f8780-cee2-11ee-ab85-456328b4c9eb',
-      name: 'John Doe',
-      email: 'john@example.com',
-      password: 'password123',
+      name: 'Mathew',
+      email: 'matthew@gmail.com',
+      password: '$2b$10$Q.Z37/8VMCI9OfQZ9PdyMuMPp7lWnNli4yZ09M5cvbiAr0F8EkiXm',
     },
     {
       id: 'f3931ee0-cee2-11ee-8f17-599eb0aa5890',
       name: 'Jane Smith',
       email: 'jane@example.com',
-      password: 'password456',
+      password: '$2b$10$L9Lz/f9C7ug0FJJls8kP5Oeq4kZSf.SjVbcZvcvVDYg0vzL1DJMSi',
     },
   ];
 
-  create(createUserDto: CreateUserDto) {
-    const newUser = { id: uuidv1(), ...createUserDto };
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = {
+      id: uuidv1(),
+      ...createUserDto,
+      password: hashedPassword,
+    };
     this.users.push(newUser);
     return newUser;
   }
