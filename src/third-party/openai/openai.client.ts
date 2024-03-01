fd4f549df89d2e95d249974a3d5da0190fa25295
tokoyamy <math.felipemachado@gmail.com>
2024-03-01 16:39:41 -0300

    Update openai.client.ts

--- src/third-party/openai/openai.client.ts
+++ src/third-party/openai/openai.client.ts
@@ -1,1 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { GentPlatAssistantePayload } from './dto/gentplat-assistante.dto';
+import { ThirdPartyUtils } from '../utils/third-party-utils';
+
+@Injectable()
+export class GenplatClient {
+  private logger: Logger;
+  private baseUrl: string;
+  private token: string;
+
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {
+    this.logger = new Logger(this.constructor.name);
+    this.baseUrl = this.configService.get('OPENAI_URL');
+    this.token = this.configService.get('REQUESTER_TOKEN');
+  }
+
+  private buildHeader() {
+    return {
+      'Content-Type': 'application/json',
+      'x-requester-token': `${this.token}`,
+      'OpenAI-Beta': 'assistants=v1',
+    };
+  }
+
+  async chat(contentUser: string, modelGpt: string): Promise<any> {
+    this.logger.log(`user question to gpt`);
+    try {
+      const payload = JSON.stringify({
+        model: modelGpt,
+        messages: [
+          {
+            role: 'user',
+            content: contentUser,
+          },
+        ],
+        temperature: 0.7,
+      });
+      const result = await this.httpService
+        .post(
+          `${this.baseUrl}/api/v1/proxy/openai/v1/chat/completions`,
+          payload,
+          {
+            headers: this.buildHeader(),
+          },
+        )
+        .toPromise();
+      return result.data;
+    } catch (error) {
+      ThirdPartyUtils.defaultErrorHandler(
+        this.logger,
+        error,
+        `Error on trying getting response user`,
+      );
+    }
+  }
+
+  async createAssistant(
+    instructions: string,
+    name: string,
+    tools: string[],
+    modelGpt: string,
+  ): Promise<any> {
+    this.logger.log(`Creating genplat assistant`);
+    try {
+      const payload = {
+        instructions: instructions,
+        name: name,
+        tools: tools,
+        model: modelGpt,
+      };
+      const result = await this.httpService
+        .post(`${this.baseUrl}/api/v1/proxy/openai/v1/assistants`, payload, {
+          headers: this.buildHeader(),
+        })
+        .toPromise();
+      return result.data;
+    } catch (error) {
+      ThirdPartyUtils.defaultErrorHandler(
+        this.logger,
+        error,
+        `Error on trying to get genplat thread id`,
+      );
+    }
+  }
+
+}
