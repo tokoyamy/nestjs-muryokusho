4c28fdae7f73d831dd81f91f6403fd3fa17dd978
tokoyamy <math.felipemachado@gmail.com>
2024-03-01 16:38:14 -0300

    Update third-party-utils.ts

--- src/third-party/utils/third-party-utils.ts
+++ src/third-party/utils/third-party-utils.ts
@@ -1,1 +1,16 @@
 import { Logger, UnprocessableEntityException } from '@nestjs/common';
+
+export class ThirdPartyUtils {
+  static defaultErrorHandler(logger: Logger, error: any, message: string) {
+    try {
+      const statusCode = error.response ? error.response.status : '';
+      const responseData = error.response && error.response.data ? JSON.stringify(error.response.data) : '';
+      logger.error(error.stack)
+      logger.error(`${message} - ${error.message} - ${statusCode} - ${responseData}`);
+    } catch (error) {
+      logger.error(message)
+    } finally {
+      throw new UnprocessableEntityException(message);
+    }
+  }
+}
