ec844180fc4b012bf087cf38ee2b53185c067730
tokoyamy <math.felipemachado@gmail.com>
2024-03-01 16:37:32 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './core/user/user.module';
 import { AuthModule } from './core/auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env.local'],
+    }),
+            UserModule, AuthModule],
 })
 export class AppModule {}
