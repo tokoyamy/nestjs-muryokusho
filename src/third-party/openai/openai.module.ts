787b6c877f382b6b51b77cd42cc1e4467af8765b
tokoyamy <math.felipemachado@gmail.com>
2024-03-01 16:41:02 -0300

    Update openai.module.ts

--- src/third-party/openai/openai.module.ts
+++ src/third-party/openai/openai.module.ts
@@ -1,1 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { GenplatClient } from './genplat.client';
+
+@Module({
+  imports: [HttpModule, ConfigModule],
+  providers: [GenplatClient],
+  exports: [GenplatClient],
+})
+export class GenplatModule {}
