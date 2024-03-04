46bc3cc4d7e4ed2261645f2f6925db473591f9a4
Tokoyamy <math.felipemachado@gmail.com>
2024-03-03 23:11:49 -0300

    adjustment module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from './core/user/user.module';
 import { AuthModule } from './core/auth/auth.module';
 import { AiModule } from './core/ai/ai.module';
 import { OpenaiModule } from './third-party/openai/openai.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,16 @@ import { OpenaiModule } from './third-party/openai/openai.module';
       isGlobal: true,
       expandVariables: true,
       envFilePath: ['.env.local'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     }),
     AuthModule,
     UserModule,
