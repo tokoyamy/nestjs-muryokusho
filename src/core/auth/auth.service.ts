83909ac53afa5db8aa50f836d396521b92b8dbe1
Tokoyamy <math.felipemachado@gmail.com>
2024-03-03 23:39:04 -0300

    adding compare hash

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,12 +12,16 @@ export class AuthService {
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
     const user = await this.usersService.findUserByEmail(email);
-    if (user?.password !== pass) {
+
+    const passwordMatch = await compare(pass, user.password);
+    if (!passwordMatch) {
       throw new UnauthorizedException();
     }
     const payload = { sub: user.id, email: user.email };
-    return {
+    const resp = {
+      user: user.email,
       access_token: await this.jwtService.signAsync(payload),
     };
+    return resp;
   }
 }
